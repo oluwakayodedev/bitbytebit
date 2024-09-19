5496aaa1fec09bbe0588fe9e3e6fce6fdc65c4f4
oluwakayodedev <iammichaelkayode@gmail.com>
2024-09-19 14:23:33 +0100

    update: added cache control to store content for 24hrs

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -4,6 +4,7 @@ const Blog = require("../models/blog.model.js");
 const getBlogs = async (req, res) => {
   try {
     const blogs = await Blog.find({});
+    res.set('Cache-Control', 'public, max-age=86400');
     res.status(200).json(blogs);
   } catch (error) {
     res.status(500).json({ message: error.message });
--- server.js
+++ server.js
@@ -1,59 +1,59 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require('path');
-const bodyParser = require('body-parser');
-const blogRoute = require("./routes/blog.route.js");
-const authRoute = require("./routes/auth.route.js");
-const app = express();
+  require("dotenv").config();
+  const express = require("express");
+  const mongoose = require("mongoose");
+  const path = require('path');
+  const bodyParser = require('body-parser');
+  const blogRoute = require("./routes/blog.route.js");
+  const authRoute = require("./routes/auth.route.js");
+  const app = express();
 
-// middleware
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
-
-// routes
-app.use("/api/blogs", blogRoute);
-app.use("/api/auth", authRoute)
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-app.get('/signin', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
-app.get('/publishBlog', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'publishBlog.html'));
-});
-app.get('/blog/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'template.html'));
-});
-app.get('/view-all-blog', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'viewall.html'));
-});
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// connect to DB and run server
-mongoose
-  .connect(process.env.MONGODB_KEY)
-  .then(() => {
-    console.log("Connected!");
-    const port = process.env.PORT;
-    app.listen(port, () => {
-      console.log("Server is running on port :", port);
-    });
-  })
-  .catch(() => {
-    console.log("Connection Failed.");
+  // middleware
+  app.use(express.json());
+  app.use(bodyParser.json());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
   });
+
+
+  // routes
+  app.use("/api/blogs", blogRoute);
+  app.use("/api/auth", authRoute)
+
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+  app.get('/signin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  });
+  app.get('/publishBlog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'publishBlog.html'));
+  });
+  app.get('/blog/:id', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'template.html'));
+  });
+  app.get('/view-all-blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'viewall.html'));
+  });
+
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+
+  // connect to DB and run server
+  mongoose
+    .connect(process.env.MONGODB_KEY)
+    .then(() => {
+      console.log("Connected!");
+      const port = process.env.PORT;
+      app.listen(port, () => {
+        console.log("Server is running on port :", port);
+      });
+    })
+    .catch(() => {
+      console.log("Connection Failed.");
+    });
