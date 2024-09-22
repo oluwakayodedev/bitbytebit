0f9dcae302e3effae4c0435405e9edbdd60f4b2a
oluwakayodedev <iammichaelkayode@gmail.com>
2024-09-22 14:51:23 +0100

    fix: fixed caching issue

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -4,7 +4,6 @@ const Blog = require("../models/blog.model.js");
 const getBlogs = async (req, res) => {
   try {
     const blogs = await Blog.find({});
-    res.set('Cache-Control', 'public, max-age=86400');
     res.status(200).json(blogs);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -64,6 +63,8 @@ const deleteBlogById = async (req, res) => {
     if (!blog) {
       return res.status(404).json({ message: "Blog not found" });
     }
+
+    res.set('Cache-Control', 'no-store');
 
     res.status(200).json({ message: "Blog Deleted Successfully!" });
   } catch (error) {
